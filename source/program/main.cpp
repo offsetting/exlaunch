b8109d7bf9b0052e1786a90f81652fd127265d9a
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2025-02-20 14:29:52 -0600

    Mount SD Card

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -1,39 +1,25 @@
 #include "lib.hpp"
+#include "nn/fs.hpp"
 
-/* Define hook StubCopyright. Trampoline indicates the original function should be kept. */
-/* HOOK_DEFINE_REPLACE can be used if the original function does not need to be kept. */
-HOOK_DEFINE_TRAMPOLINE(StubCopyright) {
-
-    /* Define the callback for when the function is called. Don't forget to make it static and name it Callback. */
-    static void Callback(bool enabled) {
-
-        /* Call the original function, with the argument always being false. */
-        Orig(false);
+HOOK_DEFINE_INLINE(MountSDCard) {
+    static void Callback(exl::hook::InlineCtx* ctx) {
+        nn::fs::MountSdCardForDebug("sd");
     }
-
 };
 
+static const char VERISON[] = "Game Version: 1.0.2";
 
-/* Declare function to dynamic link with. */
-namespace nn::oe {
-    void SetCopyrightVisibility(bool);
+HOOK_DEFINE_REPLACE(GetVersionString) {
+    static const char* Callback() {
+        return VERISON;
+    }
 };
 
 extern "C" void exl_main(void* x0, void* x1) {
     /* Setup hooking environment. */
     exl::hook::Initialize();
-
-    /* Install the hook at the provided function pointer. Function type is checked against the callback function. */
-    StubCopyright::InstallAtFuncPtr(nn::oe::SetCopyrightVisibility);
-
-    /* Alternative install funcs: */
-    /* InstallAtPtr takes an absolute address as a uintptr_t. */
-    /* InstallAtOffset takes an offset into the main module. */
-
-    /*
-    For sysmodules/applets, you have to call the entrypoint when ready
-    exl::hook::CallTargetEntrypoint(x0, x1);
-    */
+    MountSDCard::InstallAtOffset(0x1eb74);
+    GetVersionString::InstallAtOffset(0x1fcc4);
 }
 
 extern "C" NORETURN void exl_exception_entry() {
